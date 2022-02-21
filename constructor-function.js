00533433f3834428420b375e12658ca6e485bb28
codnr <wnd75913@gmail.com>
2022-02-21 18:28:25 +0900

    Edit Construction-Function

--- constructor-function.js
+++ constructor-function.js
@@ -1,5 +1,5 @@
 //constructor function
-
+//생성자 함수는 보통 첫글자를 대문자로 하여 함수를 만들어 준다.
 function Item(title, price){
     //this = {}
 
@@ -16,7 +16,7 @@ function Item(title, price){
 const item1 = new Item("인형", 3000);
 const item2 = new Item("가방", 4000);
 const item3 = new Item("지갑", 5000);
- 
+ //new를 사용해서 함수를 호출하면 return 문 없이 객체를 반환시킬 수 있다.
 console.log(item1, item2, item3);
 
 item3.showPrice();
